942443d40c402eafc0346e1c4c279580e6034422
Steven <mkh.steven@gmail.com>
2025-06-15 14:25:28 +0200

    add lexicon popup when clicking on the small lexicon on the top left of the screen

--- scenes/game.gd
+++ scenes/game.gd
@@ -21,3 +21,26 @@ func _on_countdown_timer_timeout() -> void:
 		$CountdownTimer.stop()
 		$CanvasLayer/CountdownLabel.text = "Time's up!"
 		# You can also emit a signal or call a game over here
+
+# LEXICON POPUP
+func open_lexicon():
+	$CanvasLayer/LexiconPopup.visible = true
+	$CountdownTimer.set_paused(true)
+	# Optional: get_tree().paused = true if you want to pause everything else
+
+func close_lexicon():
+	$CanvasLayer/LexiconPopup.visible = false
+	$CountdownTimer.set_paused(false)
+	# Optional: get_tree().paused = false
+
+func _on_lexicon_input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		open_lexicon()
+
+func _on_close_button_pressed() -> void:
+	close_lexicon()
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		open_lexicon()
--- scripts/global.gd
+++ scripts/global.gd
@@ -1,6 +1,7 @@
 extends Node
 # Singleton for shared global data 
 var right_answers = 0 # In quiz it is called via Global.right_answers...
-var right_answers_needed = 1 # depends on difficulty setting: 1 for easy, 2 for difficult?
-var total_answers = 10 # if easy = 10 points, if difficult 20 points.
-var difficulty = "easy" # default: easy, else difficult when selected. 
+var right_answers_needed = 1 
+var total_answers = 10 
+
+var difficulty = "easy" # easy: second try during quiz, difficult: game lost. 
