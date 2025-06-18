3847c35fd0b4bbb6b7e9ba09896f691f336c91ee
Steven <mkh.steven@gmail.com>
2025-06-18 12:58:09 +0200

    If thetime hits 10 minutes game over

--- scripts/game.gd
+++ scripts/game.gd
@@ -7,3 +7,5 @@ func _ready():
 
 func _on_score_changed():
 	$UI/ScoreText.text = "Registrierte Tiere: " + str(GlobalVariables.score) + "/35"
+	if GlobalVariables.score == 35:
+		SceneSwitcher.switch_scene("res://scenes/won.tscn")
