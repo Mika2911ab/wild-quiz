7662068d5784848d6244672f84c6d30effbd127e
Levin Kaus <levinkaus@gmail.com>
2025-06-19 12:59:46 +0200

    Put some love in the story scene

--- scripts/intro_story.gd
+++ scripts/intro_story.gd
@@ -1,9 +1,19 @@
 extends Control 
 
-#const GAME = preload("res://scenes/game.tscn")
+var counter = 0
 
 func _on_button_pressed():
-	#get_tree().change_scene_to_packed(GAME)
-	SceneSwitcher.switch_scene("res://scenes/game.tscn")
-	GlobalTimer.reset_timer()
-	GlobalTimer.start_timer()
+	if counter == 4: 
+		SceneSwitcher.switch_scene("res://scenes/game.tscn")
+		GlobalTimer.reset_timer()
+		GlobalTimer.start_timer()
+	if counter == 3: 
+		$Story/StoryText.text = "⏳ Du hast 10 Minuten, um Tiere zu entdecken und ihr Wissen zu retten."
+		$Story/Button/ButtonText.text = "Los!"
+	if counter == 2: 
+		$Story/StoryText.text = "Jetzt liegt es an dir: Ein neues, magisches Lexikon ist aufgetaucht – und DU bist die einzige Hoffnung!"
+	if counter == 1: 
+		$Story/StoryText.text = "Die Tiere verschwanden. Ihr Wissen verblasste. Das große Tierlexikon wurde ausgelöscht."
+	if counter == 0:
+		$Story/StoryText.text = "Doch ein seltsamer Sturm zog auf. Kein Regen – nur Vergessenheit."
+	counter +=1
