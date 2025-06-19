ba7accbc6fea9290567c9e0cc069ba467f4a95fe
Levin Kaus <levinkaus@gmail.com>
2025-06-19 11:38:53 +0200

    Put some love into the menu

--- scripts/fox_script.gd
+++ scripts/fox_script.gd
@@ -3,7 +3,7 @@ extends AnimatedSprite2D
 signal moving_started
 signal moving_stopped
 
-@export var walk_radius := 300.0
+@export var walk_radius := 350.0
 @export var walk_speed := 30.0
 @export var state_time_range := Vector2(2.0, 3.0)
 
@@ -15,23 +15,16 @@ func _ready():
 	randomize()
 	choose_new_state()
 
-
-
 func _process(delta):
 	state_timer -= delta
-
 	if current_state == "moving":
 		move_towards_target(delta)
-
 	if state_timer <= 0:
 		choose_new_state()
-	#print("Fox position:", global_position)
-
 
 func choose_new_state():
 	var states = ["idle", "sleeping", "jumping", "moving", "look_around", "surprised", "moving", "moving", "moving"]
-	var new_state = states[randi() % states.size()]
-	set_state(new_state)
+	set_state(states[randi() % states.size()])
 
 func set_state(state: String):
 	if current_state == "moving" and state != "moving":
@@ -41,19 +34,15 @@ func set_state(state: String):
 	state_timer = randf_range(state_time_range.x, state_time_range.y)
 	play(state)
 
-	if state == "moving" or state=="jumping":
+	if state in ["moving", "jumping"]:
 		emit_signal("moving_started")
-		var random_offset = Vector2(
+		target_position = global_position + Vector2(
 			randf_range(-walk_radius, walk_radius),
 			randf_range(-walk_radius, walk_radius)
 		)
-		target_position = global_position + random_offset
 		print("New target:", target_position)
 
-
 func move_towards_target(delta):
-	var direction = (target_position - global_position).normalized()
-	global_position += direction * walk_speed * delta
-
+	global_position += (target_position - global_position).normalized() * walk_speed * delta
 	if global_position.distance_to(target_position) < 10:
 		choose_new_state()
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,23 +1,10 @@
-extends Control
+extends Node2D
 
 func _on_start_game_pressed():
-	print("Game started")
-	#SceneSwitcher.switch_scene("res://scenes/game.tscn")
-	SceneSwitcher.switch_scene("res://scenes/intro_story.tscn")
-	#GlobalTimer.reset_timer()
-	#GlobalTimer.start_timer()
-
-func _on_difficulty_pressed():
-	print("Set difficulty")
-	# Open difficulty submenu or popup
 	SceneSwitcher.switch_scene("res://scenes/set_difficulty.tscn")
-	
 
 func _on_settings_pressed():
 	print("Go into settings")
-	# Open settings panel
-	#$SettingsPanel.visible = true
 
 func _on_quit_pressed():
-	print("Quit game")
 	get_tree().quit()
